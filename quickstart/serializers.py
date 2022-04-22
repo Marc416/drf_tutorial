8ee6df4cd112fd79a894f4a3ce4bf2655b996741
Marc416 <juni416@naver.com>
2022-04-22 17:51:55 +0900

    Learn how to make drf url

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+
 # 모델들을 시리얼 라이저로 엮는거 같음
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 
 from quickstart import views
 
+# view가 아닌 viewset을 사용하므로 url 생성을 하기 위해서 라우터에 등록해야 한다.
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
