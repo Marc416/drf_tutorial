8be59abad5047c06489a870bbe413634bf294e3d
Marc416 <juni416@naver.com>
2022-04-24 23:02:47 +0900

    Tutorial 2: Requests and Responses

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/<int:pk>', views.snippet_detail),
 ]
+# format 의 제한이 없ㅍ
+urlpatterns = format_suffix_patterns(urlpatterns)
+
